0810a72dac7cbb2316c5a558e2187bd1b0e30fb7
Viktor Malieichyk <viktor.malieichyk@gmail.com>
2023-04-04 01:03:36 +0300

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Viktor Malieichyk on 04.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
